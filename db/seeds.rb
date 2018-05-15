c72253936400edea3275c3f1b0ae79098ecec624
Louise Darche <louise.darche@gmail.com>
2018-05-15 17:18:51 +0200

    this is my wikinomous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do |article|
+  article = Article.new(title: Faker::StarWars.quote, content: Faker::Lorem.paragraph)
+  article.save
+end
